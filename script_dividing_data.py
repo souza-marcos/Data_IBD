4f5a002d293ea79ceea17efd9c9354e9c193e772
Marcos Daniel <marcosnumber11@gmail.com>
2023-11-17 07:16:48 -0300

    Estados e Refinarias

--- script_dividing_data.py
+++ script_dividing_data.py
@@ -57,10 +57,112 @@ def insertProdutos():
 
     df = pd.read_sql('SELECT * FROM Produto', conn1)
 
+def insertStates():
+
+    # EstadoID, Estado, Regiao
+
+    query = '''
+
+    SELECT DISTINCT `Estado`, `Região`
+        FROM ProdutosDerivadosRefinaria
+
+    '''
+
+    df_base = pd.read_sql(query, conn)
+
+    # Add a index column
+    df_base['EstadoID'] = df_base.index
+
+    # Rename the column 'Produto' to 'Nome'
+    df_base.rename(columns={'Região': 'Regiao'}, inplace=True)
 
 
-insertProdutos()
+    # Create a new table in the database databaseRight.db
+    create_table_query = '''
 
+    CREATE TABLE Estado (
+        EstadoID INTEGER PRIMARY KEY,
+        Estado TEXT NOT NULL,
+        Regiao TEXT NOT NULL
+    );
+
+    '''
+    conn1.execute(create_table_query)
+
+    # Insert into a table Produto in the database databaseRight.db with a sql query
+    base_insert_query = '''
+    INSERT INTO Estado (EstadoID, Estado, Regiao) VALUES 
+    '''
+
+    for row in df_base.itertuples():
+        insert_query = base_insert_query + "({}, '{}', '{}')".format(row.EstadoID, row.Estado, row.Regiao)
+        print(insert_query)
+        conn1.execute(insert_query)
+
+
+    df = pd.read_sql('SELECT * FROM Estado', conn1)
+    print(df)
+
+def insertRefinarias():
+
+    # EstadoID, Estado, Regiao
+
+    # states = pd.read_sql('SELECT * FROM Estado', conn1)
+
+    conn1.execute('DROP TABLE IF EXISTS Refinaria;')
+
+    query = '''
+
+    SELECT `CNPJ`, `Instalação`, `Razão Social`, `Município`
+        FROM ProdutosDerivadosRefinaria
+        GROUP BY `CNPJ`
+    '''
+
+    df_base = pd.read_sql(query, conn)
+
+    # Add a index column
+    df_base['EmpID'] = df_base.index
+
+    # Rename the column 'Produto' to 'Nome'
+    df_base.rename(columns={'Razão Social': 'RazaoSocial'}, inplace=True)
+    df_base.rename(columns={'Instalação': 'Instalacao'}, inplace=True)
+    df_base.rename(columns={'Município': 'Municipio'}, inplace=True)
+
+
+    foreignKeyconstraint = 'PRAGMA foreign_keys = 0;'
+    conn1.execute(foreignKeyconstraint)
+
+    # Create a new table in the database databaseRight.db
+    create_table_query = '''
+
+    CREATE TABLE Refinaria (
+        EmpID INTEGER PRIMARY KEY,
+        CNPJ TEXT NOT NULL,
+        Instalacao TEXT NOT NULL,
+        RazaoSocial TEXT NOT NULL,
+        Municipio TEXT NOT NULL,
+        FOREIGN KEY (EmpID) REFERENCES Empresa (EmpID)
+    );
+
+    '''
+    conn1.execute(create_table_query)
+
+    # Insert into a table Produto in the database databaseRight.db with a sql query
+    base_insert_query = '''
+    INSERT INTO Refinaria (EmpID, CNPJ, Instalacao, RazaoSocial, Municipio) VALUES
+    '''
+
+    for row in df_base.itertuples():
+        insert_query = base_insert_query + "({}, '{}', '{}', '{}', '{}')".format(row.EmpID, row.CNPJ, row.Instalacao, row.RazaoSocial, row.Municipio) 
+        print(insert_query)
+        conn1.execute(insert_query)
+
+
+    df = pd.read_sql('SELECT * FROM Refinaria', conn1)
+    print(df)
+
+# insertProdutos()
+insertRefinarias()
 
 
 conn.close()
