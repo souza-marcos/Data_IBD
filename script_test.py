e383696e40315dab5cf61ce3d26f366cea754ce5
Marcos Daniel <marcosnumber11@gmail.com>
2023-11-17 22:12:39 -0300

    Removed unused scripts

--- script_test.py
+++ script_test.py
@@ -2,13 +2,23 @@ import pandas as pd
 # Connection with the database sqlite3
 import sqlite3
 conn1 = sqlite3.connect('databaseRight.db')
+conn = sqlite3.connect('database.db')
 
-query = ''' 
-SELECT * FROM Produto
-'''
+# query = '''
 
-df = pd.read_sql(query, conn1)
-print(df)
+#     SELECT `Mês/Ano`, `CNPJ`, `Capacidade Autorizada (m³/d)`
+#         FROM ProcessamentoPetroleo
+#     '''
 
+# df_base = pd.read_sql(query, conn)
+
+# df_base['Mês/Ano'] = pd.to_datetime(df_base['Mês/Ano'], format='%m/%Y')
+
+# # Group by 'CNPJ' and 'Capacidade Autorizada (m³/d)', and then aggregate using the first 'Mês/Ano'
+# result_df = df_base.groupby(['CNPJ', 'Capacidade Autorizada (m³/d)']).agg({'Mês/Ano': 'first'})
+
+result_df = pd.read_sql('SELECT COUNT(*) FROM PRODUCAO ', conn1)
+
+print(result_df)
 
 conn1.close()
